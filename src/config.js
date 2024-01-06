b984a86a43b6654127bd123be00d99988964315d
Abdirizak abdullahi hussein <102373033+Alaaja8899@users.noreply.github.com>
2024-01-06 19:22:18 +0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -3,13 +3,14 @@ import { getAuth ,GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyD-V97FbwQBbLIkbvn98P614nlkKSoGXUk",
-  authDomain: "chatt-room.vercel.app",
+  authDomain: "auth-979d8.firebaseapp.com",
   databaseURL: "https://auth-979d8-default-rtdb.firebaseio.com",
   projectId: "auth-979d8",
   storageBucket: "auth-979d8.appspot.com",
   messagingSenderId: "235770544453",
   appId: "1:235770544453:web:79093a7c45b3e323ca7e2c"
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
